7146d810fc36ee3fa4a34f6cd2692b703972378d
Sarah Zaman <szaman@u.rochester.edu>
2020-09-10 01:56:40 -0400

    hover

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,40 +2,65 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 	// create "handles" on the buttons from the HTML
-	var _______________ = document.getElementById("_______________");
-	var _______________ = document.getElementById("_______________");
+	var ingredientNextButton = document.getElementById("ingredientNext");
+	var stepsNextButton = document.getElementById("stepNext");
 
 	// create "handles" on the lists
-	var _______________ = document.getElementById("_______________");
-	var _______________ = document.getElementById("_______________");
+	var ingredientsList = document.getElementById("ingredientsList");
+	var stepsList = document.getElementById("stepList");
 
 	// get the lengths of the lists
-	_______________ = _______________.children.length;
-	_______________ = _______________.children.length;
+	var ingredientsListLength = ingredientsList.children.length;
+	var stepsListLength = stepsList.children.length;
 
 	// for each item in the lists, set their display value to "none" (hide them)
-	for (var i = 0; i < _______________; i++) {
-		_______________.children[i].style.display="none";
+	for (var i = 0; i < ingredientsListLength; i++) {
+		ingredientsList.children[i].style.display="none";
 	}
-	for (var i = 0; i < _______________; i++) {
-		_______________.children[i].style.display="none";
+	for (var i = 0; i < stepsListLength; i++) {
+		stepsList.children[i].style.display="none";
 	}
+	//calling function
+	ingredientShowsNext(ingredientsListLength,ingredientsList,ingredientNextButton);
+	//calling function for steps
+	stepsShowNext(stepsListLength,stepsList,stepsNextButton);
 
-	// setup a counter for the list
-	_______________ = 0;
-	// wait for the user to click the button, then "revert" (show item) the next
-	//. item in the list
-	_______________.addEventListener("click", function() {
-		_______________.children[_______________].style.display="revert";
-		_______________++;
-	});
+	//for showing description
+	var aboutHead = document.getElementById("aboutHead");
+	var description = document.getElementById("description");
+	showDescription(aboutHead,description);
 
-	// setup a counter for the list
-	_______________ = 0;
-	// wait for the user to click the button, then "revert" (show item) the next
-	//. item in the list
-	_______________.addEventListener("click", function() {
-		_______________.children[_______________].style.display="revert";
-		_______________++;
-	});
 });
+
+// function to wait for the user to click the button, then "revert" (show item) the next
+function ingredientShowsNext(ingredientsListLength, ingredientsList, ingredientNextButton){
+	var ingredientsListHead = 0;
+	ingredientNextButton.addEventListener("click", function () {
+		if(ingredientsListHead < ingredientsListLength){
+			ingredientsList.children[ingredientsListHead].style.display="revert";
+		}
+		ingredientsListHead++;
+	});
+}
+
+//function to wait for user to click button and reverting the item for steps
+function stepsShowNext(stepsListLength, stepsList, stepsNextButton){
+	var stepsListHead = 0;
+	stepsNextButton.addEventListener("click",function () {
+		if(stepsListHead<stepsListLength){
+			stepsList.children[stepsListHead].style.display="revert";
+		}
+		stepsListHead++;
+	})
+}
+
+function showDescription(aboutHead, description) {
+	description.style.display="none";
+	aboutHead.addEventListener("mouseenter", function () {
+		description.style.display="revert";
+	})
+	aboutHead.addEventListener("mouseleave",function () {
+		description.style.display="none";
+	})
+}
+
